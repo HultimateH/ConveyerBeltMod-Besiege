e8c180a4d27f64ea74ddb8ada5e5c80b7c273521
Tomee <1014723833@qq.com>
2022-03-12 17:12:09 +0800

    常规提交

--- ConveyerBeltMod/ConveyerBeltBlock/ConveyerBeltBlockScript.cs
+++ ConveyerBeltMod/ConveyerBeltBlock/ConveyerBeltBlockScript.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConveyerBeltMod
+namespace ConveyerBeltMod.ConveyerBeltBlock
 {
-    public class Class1
+    class ConveyerBeltBlockScript
     {
     }
 }
---
+++ ConveyerBeltMod/Mod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+
+namespace ConveyerBeltMod
+{
+    public class Mod:ModEntryPoint
+    {
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+    }
+}
