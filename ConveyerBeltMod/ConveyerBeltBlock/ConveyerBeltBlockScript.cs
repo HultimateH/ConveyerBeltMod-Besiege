e6586101f3c4819d282469454b72d71461b17195
Tomee <1014723833@qq.com>
2022-03-12 23:37:32 +0800

    完成基本功能 添加speed滑条 更改版本号

--- ConveyerBeltMod/ConveyerBeltBlock/ConveyerBeltBlockScript.cs
+++ ConveyerBeltMod/ConveyerBeltBlock/ConveyerBeltBlockScript.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 class ConveyerBeltBlockScript : BlockScript
 {
+    MSlider speedSlider,frictionSlider;
+    
     [SerializeField]
     private GameObject plain1, plain2;
     [SerializeField]
@@ -14,16 +16,18 @@ class ConveyerBeltBlockScript : BlockScript
 
     public override void SafeAwake()
     {
-        base.SafeAwake();
+        speedSlider = AddSlider("speed", "speed", 1f, 0f, 5f);
+
+
     }
 
     public override void OnBlockPlaced()
     {
-        plain1 = createPlain("Plain 1", new Vector3(0f, /*0.1875f*/0.23f, 0.75f));
-        //plain2 = createPlain("Plain 2", new Vector3(0f, -0.1875f, 0.75f));
-        //cylinder1 = createCylinder("Cylinder 1", new Vector3(1.25f, 0f, 0.75f));
-        //cylinder2 = createCylinder("Cylinder 2", new Vector3(-1.25f, 0f, 0.75f));
-        //GetComponent<Rigidbody>().centerOfMass = plain1.transform.localPosition;
+        plain1 = createPlain("Plain 1", new Vector3(0f, 0.2f, 0.75f));
+        plain2 = createPlain("Plain 2", new Vector3(0f, -0.2f, 0.75f));
+        cylinder1 = createCylinder("Cylinder 1", new Vector3(1.25f, 0f, 0.75f));
+        cylinder2 = createCylinder("Cylinder 2", new Vector3(-1.25f, 0f, 0.75f));
+
         GameObject createPlain(string name,Vector3 localPosition)
         {
             var go = new GameObject(name);
@@ -32,11 +36,13 @@ class ConveyerBeltBlockScript : BlockScript
 
             var _mc = go.AddComponent<MeshCollider>();
             _mc.sharedMesh = ModResource.GetMesh("plain_mesh");
-            _mc.convex = true;
             BlockBehaviour.myBounds.childColliders.Add(_mc);
-            Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), _mc);
+            _mc.material.dynamicFriction = 1f;
+            _mc.material.staticFriction = 1f;
+            _mc.material.frictionCombine = PhysicMaterialCombine.Maximum;
+            //Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), _mc);
 
-            go.AddComponent<Rigidbody>()/*.isKinematic = true*/;
+            go.AddComponent<Rigidbody>().isKinematic = true;
             go.transform.SetParent(transform);
             go.transform.rotation = transform.rotation;
             go.transform.localPosition = localPosition;
@@ -51,26 +57,44 @@ class ConveyerBeltBlockScript : BlockScript
 
             var _mc = go.AddComponent<MeshCollider>();
             _mc.sharedMesh = ModResource.GetMesh("cylinder_mesh");
-            _mc.convex = true;
             BlockBehaviour.myBounds.childColliders.Add(_mc);
-            Physics.IgnoreCollision(_mc, GetComponentInChildren<BoxCollider>());
+            _mc.material.dynamicFriction = 1f;
+            _mc.material.staticFriction = 1f;
+            _mc.material.frictionCombine = PhysicMaterialCombine.Maximum;
+            //Physics.IgnoreCollision(_mc, GetComponentInChildren<BoxCollider>());
 
-            //go.AddComponent<Rigidbody>().isKinematic = true;
+            go.AddComponent<Rigidbody>().isKinematic = true;
             go.transform.SetParent(transform);
             go.transform.rotation = transform.rotation;
             go.transform.localPosition = localPosition;
 
             return go;
         }
+
     }
 
-    public override void OnSimulateStart()
+
+    public override void SimulateFixedUpdateAlways()
     {
-        Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), plain1.GetComponent<MeshCollider>());
-        plain1.GetComponent<Rigidbody>().isKinematic = true;
-        //Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), plain2.GetComponent<MeshCollider>());
-        //Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), cylinder1.GetComponent<MeshCollider>());
-        //Physics.IgnoreCollision(GetComponentInChildren<BoxCollider>(), cylinder2.GetComponent<MeshCollider>());
+        var rigid = plain1.GetComponent<Rigidbody>();
+        Vector3 pos = rigid.position;
+        Vector3 temp = -transform.right * (BlockBehaviour.Flipped ? 1f : -1f) * speedSlider.Value * Time.fixedDeltaTime;
+        rigid.position += temp;
+        rigid.MovePosition(pos);
+
+        var rigid1 = cylinder1.GetComponent<Rigidbody>();
+        Quaternion rot = rigid1.rotation;
+        Quaternion angle = Quaternion.AngleAxis(speedSlider.Value * Time.fixedDeltaTime, cylinder1.transform.forward * (BlockBehaviour.Flipped ? 1f : -1f));
+        rigid1.rotation *= angle;
+        rigid1.MoveRotation(rot);
+
+        var rigid2 = cylinder2.GetComponent<Rigidbody>();
+        Quaternion rot1 = rigid2.rotation;
+        Quaternion angle1 = Quaternion.AngleAxis(speedSlider.Value * Time.fixedDeltaTime, cylinder2.transform.forward * (BlockBehaviour.Flipped ? 1f : -1f));
+        rigid2.rotation *= angle1;
+        rigid2.MoveRotation(rot1);
     }
+
+
 }
 
